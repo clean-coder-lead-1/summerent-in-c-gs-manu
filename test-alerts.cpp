95fad84a95a628ed6c25c6226c43b10a868ce9cc
gs-manu <67756529+gs-manu@users.noreply.github.com>
2022-03-31 22:43:57 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,7 +3,16 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
+TEST_CASE("infers the breach according to limits") 
+{
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
   REQUIRE(inferBreach(50, 20, 30) == TOO_HIGH);
+  
 }
+TEST_CASE("Classifies the breach according to type of cooling") 
+{
+	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 20) == NORMAL);
+	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -1) == TOO_LOW);
+	REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 50) == TOO_HIGH);
+}
+  
